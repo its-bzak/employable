94c80b8e21fb4cdf0703c27f938f02a1a5d405e7
Ryan B <barczakryan@outlook.com>
2025-11-15 16:30:55 -0500

    changed required fields for signup / registration

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +11,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
+        fields = ('username', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(

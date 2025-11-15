44dfb4c3c9ee6d0cf0e328d8227c343741467794
Ryan B <barczakryan@outlook.com>
2025-11-15 14:35:30 -0500

    Created serializers.py and created serializers for each new model

---
+++ backend/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import *
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('__all__')
+
+class CompanyProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyProfile
+        fields = ('__all__')
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ('__all__')
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = ('__all__')
+
+class WorkExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkExperience
+        fields = ('__all__')
